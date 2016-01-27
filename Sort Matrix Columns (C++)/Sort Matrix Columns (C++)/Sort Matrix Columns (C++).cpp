0d67a47f3f7d0ccb0925d8fb172e107412371969
danceformewayne <danceforme_wayne@hotmail.com>
2016-01-27 14:27:48 -0500

    Sort Matrix Columns: Input done

--- Sort Matrix Columns (C++)/Sort Matrix Columns (C++)/Sort Matrix Columns (C++).cpp
+++ Sort Matrix Columns (C++)/Sort Matrix Columns (C++)/Sort Matrix Columns (C++).cpp
@@ -5,60 +5,78 @@
 // you should sort it by the lowest number of second line, 
 // if numbers in the second line are also equal you should sort it by the next row etc.
 
+/*Input Sample: 
+1 2 3 | 4 5 6 | 7 8 9
+*/
+
 #include "stdafx.h"
 #include <iostream>
 #include <vector> //construct matrix
 #include <regex>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
-vector<vector<int>> create_matrix(string line); //row<column>
+void sort_matrix(vector<vector<int>> matrix);
 
 int main()
 {
+
 	vector<vector<int>> matrix;
 
 	string line;
 	vector<int> row;
+	bool isDone = false;
 
-	/*
-	while (getline(cin, line, ' ')) {
-		if (line == "|") {
-			matrix.push_back(row);
-			vector<int> row; //new instance of row
+	while (getline(cin, line)) {
+		istringstream iss(line);
+		string input; 
+		if (line.empty()) 
+			break;
+
+		while (iss >> input) {
+			if (input == "|") {
+				matrix.push_back(row); //add row
+				row.clear(); //clear list to restart
+			}
+			else {
+				int num = stoi(input);
+				row.push_back(num);
+			}
 		}
-		else {
-			int num = stoi(line); //convert string to integer
-			row.push_back(num); // add integer to row vector
-		}
+		matrix.push_back(row); //push back the last one
 	}
-	cout << matrix[0][0] << matrix[0][1] << matrix[0][2]; */
-	vector<string> test;
+
+	cout << matrix[0][0] << matrix[0][1] << matrix[0][2] << endl;
+	cout << matrix[1][0] << matrix[1][1] << matrix[1][2] << endl;
+	cout << matrix[2][0] << matrix[2][1] << matrix[2][2] << endl;
+ 
 
 	//need to send EOL character 
-	while (getline(cin, line, ' ')) {
-		test.push_back(line);
-		cout << line;
-	}
-
-	for each (string var in test) {
-		cout << var;
-	}
-
 	int num;
 	cin >> num;
 
     return 0;
 }
 
-vector<vector<int>> create_matrix(string line) {
-
-	vector<vector<int>> matrix;
-
-	//generate row vector
-	vector<int> row;
+void sort_matrix(vector<vector<int>> matrix) {
 	
+	int i = 0;
+	int j = 0; 
+
+	bool isSorted = false;
+	while (!isSorted) {
+		for (int j = 0; j < matrix[j].size; j++) {
+			
+		}
+	}
+	for (int i = 0; i < matrix.size(); i++) {
+	}
+
+	for each(int var in matrix[j]) {
 		
-	return matrix;
+		//pass
+	}
+	//sort the matrix. Should be dynamic array
 }
