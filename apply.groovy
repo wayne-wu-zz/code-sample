62b79d327c3f044e7cd2fedb9645773d7dbce485
Wayne Wu <danceforme_wayne@hotmail.com>
2016-06-02 22:44:00 +0800

    Create apply.groovy

---
+++ apply.groovy
@@ -0,0 +1,19 @@
+buildscript{
+    repositories{
+        maven{
+            url "https://plugins.gradle.org/m2/"
+        }
+        maven{
+            url 'http://dl.bintray.com/cjstehno/public'
+        }
+    }
+    dependencies{
+        classpath group: 'gradle.plugin.com.yahoo.parsec.parsec-templates',
+                name: 'parsec-templates',
+                version: '1.1-SNAPSHOT'
+    }
+}
+
+if(!project.plugins.findPlugin(com.yahoo.parsec.paresc_templates.ParsecTemplatesPlugin)){
+    project.apply(plugin: com.yahoo.parsec.parsec_templates.ParsecTemplatesPlugin)
+}
